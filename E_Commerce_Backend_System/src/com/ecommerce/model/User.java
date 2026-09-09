4f92f148c30abe26b7b81a903713d0825ef99c85
MD Sonu Sekh <ssekh6920@gmail.com>
2026-09-10 02:16:34 +0530

    Just Started The implementation of User class

---
+++ E_Commerce_Backend_System/src/com/ecommerce/model/User.java
@@ -0,0 +1,43 @@
+package com.ecommerce.model;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+   public User(String name, String email , String password, String role)
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
+}
